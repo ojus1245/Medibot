f8e587ac161853ca7e41468924f1682e295f695b
ojus1245 <ojuspatil11@gmail.com>
2025-09-10 11:23:46 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+list_of_files= [
+     "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+   " test.py"
+]
+
+for file_path in list_of_files:
+    file_path=Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir:
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Created directory: {file_dir}")
+    
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, 'w') as f:
+            pass
+        logging.info(f"Created empty file: {file_path}")
+
+    else:
+        logging.info(f"File already exists and is not empty: {file_path}")
